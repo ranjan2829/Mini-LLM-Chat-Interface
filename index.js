72ec174c38abaa2f2180ca2a7c6ab61db0b954b9
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2024-11-02 15:18:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from "express";
 import Groq from "groq-sdk";
 import path from "path";
-import { fileURLToPath } from "url";
+
 import dotenv from "dotenv";
 
 // Load environment variables from .env file
 dotenv.config();
 
-// Get the directory name from the module URL
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 const app = express();
 app.use(express.json());
