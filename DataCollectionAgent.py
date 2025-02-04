aa67496122e1b21c76d845de06f4925a4296367e
ranjan2829 <ranjan.shitole3129@gmail.com>
2025-02-04 17:33:16 +0530

    data collection prototype

--- DataCollectionAgent.py
+++ DataCollectionAgent.py
@@ -1,3 +1,11 @@
 #Data Collection AI Agent
-import dotenv
+
+
 import requests
+
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=B78G3U75VXNUVEDS'
+r = requests.get(url)
+data = r.json()
+
+print(data)
